e42554d60f1fa80e5c6d7e80a8344ce81577939a
ma-558 <58020381+ma-558@users.noreply.github.com>
2019-12-18 17:12:51 +0900

    deployやで。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+  def index
+    render html: "hello world"
+  end
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'Hello World' && password == '6565'
+    end
+  end
 end
